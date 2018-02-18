f335bd53011aeba378d3b42a36a0935434262bc2
deoism <deoism@gmail.com>
2018-02-18 00:22:31 -0500

    first commit stable environment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            DeoisM React Configured
+         </div>
+      );
+   }
+}
+export default App;
